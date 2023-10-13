79ea36a3d1a23ebabce5341f4b9bc2d201d0c69b
vikas <vicky582050@gmail.com>
2023-10-13 19:55:49 +0530

    login commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+// import { Counter } from './features/counter/Counter';
+import './App.css';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home></Home>,
+  },
+  {
+    path: "/login",
+    element:<LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element:<SignUpPage></SignUpPage>,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App ">
+<RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
